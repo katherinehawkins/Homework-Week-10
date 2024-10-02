79b39c42224bcf6782a4074bcfab8a01e84d0f2c
Katherine Hawkins <khaw0004@student.monash.edu>
2024-10-02 18:07:57 +1000

    first commit

---
+++ bubble_chart.js
@@ -0,0 +1,8 @@
+var vg_1 = "homework_w10_31561764.json";
+var vg_2 = "homework_w9_31561764.json";
+vegaEmbed("#bubble_chart", vg_1).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#map_chart", vg_2).then(function(result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
